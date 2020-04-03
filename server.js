230c0fc16ff9e0812cc4af0dd237c925e2660e5f
himanshu789-hub <60208368+himanshu789-hub@users.noreply.github.com>
2020-04-03 11:40:21 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -8,37 +8,37 @@ app.use(cors())
 
 app.get('/getProfileImage/:upload/:fileName', (req, res) => {
 
-    try {
-        console.log(req.params)
-        let filePath = req.params.upload + '/' + req.params.fileName;
-        console.log('GetImage', filePath)
-        return res.sendFile(`${__dirname}/` + filePath);
-    } catch (err) {
-        console.log(err)
-        return res.status(500).send(err)
-    }
+  try {
+
+    let filePath = req.params.upload + '/' + req.params.fileName;
+
+    return res.sendFile(`${__dirname}/` + filePath);
+  } catch (err) {
+
+    return res.status(500).send(err)
+  }
 });
-console.log('watching')
+
 app.post('/upload', (req, res) => {
 
-    var file = req.files.file;
+  var file = req.files.file;
 
-    console.log('Selected File ', file)
-    if (!req.files)
-        return res.status(400).json({ msg: 'File Not Selected' })
-    console.log('NUll ? ', file == null)
-    file.name = file.md5 + '.png'
-    console.warn(`${__dirname}\\uploads\\${file.name}`)
-    file.mv(`${__dirname}\\uploads\\${file.name}`, err => {
-        if (err) {
-            console.log(err);
-            return res.status(500).json({ msg: 'file directory not exists' })
-        }
-        console.warn('-->UploadedFile : ', file);
-        return res.status(200).json({ fileName: file.name, filePath: '/uploads/' + `${file.name}` })
 
-    });
+  if (!req.files)
+    return res.status(400).json({ msg: 'File Not Selected' })
+
+  file.name = file.md5 + '.png'
+  console.warn(`${__dirname}\\uploads\\${file.name}`)
+  file.mv(`${__dirname}\\uploads\\${file.name}`, err => {
+    if (err) {
+
+      return res.status(500).json({ msg: 'file directory not exists' })
+    }
+    console.warn('-->UploadedFile : ', file);
+    return res.status(200).json({ fileName: file.name, filePath: '/uploads/' + `${file.name}` })
+
+  });
 
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'))
+app.listen(5000, () =>
